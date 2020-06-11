d8f82dc5913add8031b76d0f1e59be8fac7b3ad6
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-12 00:41:57 +0900

    登録機能の実装
    元のデータを操作するために、tasks・setTasksをpropsで受け取れるように設定する、そのためのTypeを設定する。

--- src/components/TaskInput/TaskInput.tsx
+++ src/components/TaskInput/TaskInput.tsx
@@ -1,11 +1,22 @@
 import React,{ useState } from 'react'
 import {Task} from '../../Types'
 
-const TaskInput :React.FC = () => {
+type Props = {
+  tasks: Task[]
+  setTasks:React.Dispatch<React.SetStateAction<Task[]>>
+}
+
+
+const TaskInput :React.FC<Props> = ({ tasks, setTasks}) => {
   const [ inputTitle, setInputTitle] = useState('')
+  const [ count, setCount] = useState(tasks.length + 1)
 
   const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setInputTitle(e.target.value)
+  }
+
+  const handleSubmit = () {
+    
   }
   return (
     <div className="input-from">
